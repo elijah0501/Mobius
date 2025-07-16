ba5e5d5b1a3d5de59e82d429dbb9ff4098ce5e2e
Elijah <elijah0501@outlook.com>
2025-07-16 17:55:38 +1200

    update vite setting

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/Mobius/',
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+
+// })
+
+
+export default defineConfig(({ command }) => ({
+  // 根据构建命令设置基础路径
+  base: command === 'build' ? '/Mobius/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +32,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
